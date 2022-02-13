775ca3aab4cb880638bebd72ab9353258fae925e
franky-tampubolon <50657612+satuuntuksemuaaja@users.noreply.github.com>
2022-02-14 06:35:37 +0700

    add menu rekap vendor

---
+++ app/Exports/VendorExport.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Exports;
+
+use Illuminate\Contracts\View\View;
+use Maatwebsite\Excel\Concerns\FromView;
+use Maatwebsite\Excel\Concerns\WithStyles;
+use Maatwebsite\Excel\Concerns\WithTitle;
+use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
+use PhpOffice\PhpSpreadsheet\Style\Font;
+use PhpOffice\PhpSpreadsheet\Style\NumberFormat;
+use PhpOffice\PhpSpreadsheet\Style\Border;
+use Illuminate\Support\Str;
+use Maatwebsite\Excel\Concerns\WithColumnWidths;
+
+class VendorExport implements WithTitle, WithStyles, WithColumnWidths
+{
+    protected $data;
+
+    public function __construct(array $data)
+    {
+        $this->data = $data;
+    }
+
+    public function styles(WorkSheet $sheet)
+    {
+        $data = $this->data;
+        $row = 1;
+        foreach($data as $warna => $value)
+        {
+            $sheet->getStyle('A'.$row.':E'.$row)->getFont()->setSize(12);
+            $sheet->getStyle('A'.$row.':E'.$row)->getAlignment()->setWrapText(true);                
+            $sheet->getStyle('A'.$row.':E'.$row)->getNumberFormat()->setFormatCode(NumberFormat::FORMAT_NUMBER);
+            $sheet->getStyle('A'.$row.':E'.$row)->getAlignment()->setVertical('center');
+            $sheet->getStyle('A'.$row.':E'.$row)->getAlignment()->setHorizontal('center');
+            $sheet->getStyle('A'.$row.':E'.$row)->getBorders()->getAllBorders()->setBorderStyle(Border::BORDER_THIN);
+            $sheet->getRowDimension($row)->setRowHeight(32);
+            $sheet->getCell('A'.$row)->setValue('No Map');
+            $sheet->getCell('B'.$row)->setValue('No BTD');
+            $sheet->getCell('C'.$row)->setValue('Nominal');
+            $sheet->getCell('D'.$row)->setValue('PCA');
+            $sheet->getCell('E'.$row)->setValue('WARNA MAP');
+
+            $row++; //baris ke 2
+            foreach($value as $nominal => $b){
+                $sheet->getStyle('A'.$row.':E'.$row)->getFont()->setSize(12);
+                $sheet->getStyle('A'.$row.':E'.$row)->getAlignment()->setWrapText(true);
+                $sheet->getStyle('A'.$row.':E'.$row)->getNumberFormat()->setFormatCode(NumberFormat::FORMAT_NUMBER);
+                $sheet->getStyle('A'.$row.':E'.$row)->getAlignment()->setVertical('center');
+                $sheet->getStyle('A'.$row.':E'.$row)->getAlignment()->setHorizontal('center');
+                $sheet->getStyle('A'.$row.':E'.$row)->getBorders()->getAllBorders()->setBorderStyle(Border::BORDER_THIN);
+                $sheet->getRowDimension($row)->setRowHeight(32);
+                $sheet->getCell('B'.$row)->setValue($b['no_btd']);
+                $sheet->getCell('C'.$row)->setValue(number_format($b['amount'], 0, ',', '.'));
+                $sheet->getCell('D'.$row)->setValue(Str::upper($b['pca']));
+                $sheet->getCell('E'.$row)->setValue(Str::upper($b['warna_map']));
+                $row++; //cetak baris selanjutnya
+            }
+            $sheet->getStyle('A'.$row.':E'.$row)->getFont()->setSize(12);
+            $sheet->getStyle('A'.$row.':E'.$row)->getAlignment()->setWrapText(true);
+            $sheet->getStyle('A'.$row.':E'.$row)->getAlignment()->setVertical('center');
+            $sheet->getStyle('A'.$row.':E'.$row)->getAlignment()->setHorizontal('center');
+            $sheet->getStyle('A'.$row.':E'.$row)->getBorders()->getAllBorders()->setBorderStyle(Border::BORDER_THIN);
+            $sheet->getRowDimension($row)->setRowHeight(32);
+            $row++;
+            $sheet->getStyle('A'.$row.':E'.$row)->getFont()->setSize(12);
+            $sheet->getStyle('A'.$row.':E'.$row)->getAlignment()->setWrapText(true);
+            $sheet->getStyle('A'.$row.':E'.$row)->getAlignment()->setVertical('center');
+            $sheet->getStyle('A'.$row.':E'.$row)->getAlignment()->setHorizontal('center');
+            $sheet->getStyle('A'.$row.':E'.$row)->getBorders()->getAllBorders()->setBorderStyle(Border::BORDER_THIN);
+            $sheet->getRowDimension($row)->setRowHeight(32);
+            $row++;
+        }
+        $sheet->getPageSetup()->setPrintArea('A1:E'.$row);
+        $sheet->getPageSetup()->setFitToPage(true);
+    }
+
+    public function columnWidths(): array
+    {
+        return [
+            'A' => 11,
+            'B' => 20.86,
+            'C' => 19.29,
+            'D' => 26.43,
+            'E' => 14          
+        ];
+    }
+
+    public function title(): string
+    {
+        return date('dnY');
+    }
+}
--- app/Http/Controllers/RekapBaruController.php
+++ app/Http/Controllers/RekapBaruController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Exports\RekapBaruExport;
+use App\Exports\VendorExport;
 use App\Imports\KebunImport;
 use App\Imports\RekapImport;
 use Illuminate\Support\Str;
@@ -27,6 +28,9 @@ class RekapBaruController extends Controller
         }else{
             $data = $this->import_vendor($request->file('excel'));
             $jenis = 'Vendor';
+            $name_file = $request->file('excel')->getClientOriginalName();
+            $export = new VendorExport($data, $name_file);
+            return Excel::download($export, 'Rekap_'.$name_file.'.xlsx');
         }
         // dd($data);
         $new_data = [];
@@ -98,6 +102,79 @@ class RekapBaruController extends Controller
         return $data;
     }
 
+    protected function import_vendor($file)
+    {
+        $data = [];
+        $datas = Excel::toArray(new RekapImport, $file);
+        $new = Arr::except($datas[0], [0]);
+        $collections = collect($new)->groupBy([23]);
+        // dd($collections);
+        foreach($collections as $key => $row)
+        {
+            // cek HO atau tidak
+            $ho = explode(',', $row[0][21])[0];
+            if(strtoupper($ho) === 'HO'){
+                // echo $ho;
+                $map = 'Biru';
+            }else{
+                $due_date = Carbon::parse(\PhpOffice\PhpSpreadsheet\Shared\Date::excelToDateTimeObject($row[0][8]));
+                $map = $this->cek_map($due_date);
+                // dd($map);
+            }
+
+            // cek PCA index 1
+            $company_code = (int) $row[0][0];
+            $pca = $row[0][1];
+            $pca = $this->cek_pca($pca, $company_code);
+
+            // cek nominal
+            $amount = (int) Str::after($row[count($row)-1][10], '-');
+            $amount = $this->cek_nominal($amount);
+            if($amount === 'kurang 200 juta'){
+                // cek apakah PO atau tidak melalui Purchasing Document
+                if($row[0][6]){
+                    $jenis_po = 'PO';
+                }else{
+                    $jenis_po = 'Non PO';
+                }
+                $data[$map][$amount][$jenis_po][] = [
+                    'warna_map' => $map,
+                    'no_btd' => $key,
+                    'amount' => (int) Str::after($row[count($row)-1][10], '-'),
+                    'pca' => $pca .' - '.$jenis_po
+                ];
+            }else{
+                $data[$map][$amount][] = [
+                    'warna_map' => $map,
+                    'no_btd' => $key,
+                    'amount' => (int) Str::after($row[count($row)-1][10], '-'),
+                    'pca' => $pca
+                ];
+            }
+        }
+        // dd($data);
+        $new_data = [];
+        foreach($data as $warna => $a)
+        {
+            // dd($a);
+            foreach($a as $nominal => $b)
+            {
+                
+                if($nominal === 'kurang 200 juta'){
+                    foreach($b as $po => $c)
+                    {
+                        $new_data[] = $c;
+                    }
+                }else{
+                    $new_data[] = $b;
+                }
+                
+            }
+        }
+        // dd($new_data);
+        return $new_data;
+    }
+
     protected function export_excel($data, $jenis, $name_file)
     {
         // $name_file = Str::after($name_file, 'TINA_');
@@ -113,6 +190,48 @@ class RekapBaruController extends Controller
             return '200 juta - 1 milyar';
         }else{
             return 'diatas 1 milyar';
+        }
+    }
+
+    protected function cek_map($due_date)
+    {
+        $now = Carbon::now();
+        if($due_date ->lessThanOrEqualTo($now->addDays(1))){
+            return 'Kuning' ; //bayar besok
+        }else if($due_date->greaterThanOrEqualTo($now->addDays(2))){
+            return 'Hijau'; //bayar lusa ke atas
+        }
+    }   
+
+    protected function cek_pca($pca, int $company_code)
+    {
+        $Trading = ['L', 'M', 'E'];
+        $Surabaya = ['R124', 'P201', 'C201', 'P206', 'C206'];
+        $Marunda = ['R120', 'P200', 'C200', 'P205', 'C205'];
+        $Medan = ['P202', 'C202', 'P207', 'C207'];
+        $Consumer = ['C203'];
+        $Tarjun = ['R130'];
+        $Sbe = ['E100', 'R521'];
+
+        // untuk SOCI
+        if($company_code === 5600){
+            return 'SOCI';
+        }
+
+        if(in_array(substr($pca,0,1), $Trading)){
+            return 'Trading';
+        }else if(in_array($pca, $Surabaya)){
+            return 'Surabaya';
+        }else if(in_array($pca, $Marunda)){
+            return 'Marunda';
+        }else if(in_array($pca, $Medan)){
+            return 'Medan';
+        }else if(in_array($pca, $Consumer)){
+            return 'Consumer';
+        }else if(in_array($pca, $Tarjun)){
+            return 'Tarjun';
+        }else if(in_array($pca, $Sbe)){
+            return 'Sbe';
         }
     }
 }
