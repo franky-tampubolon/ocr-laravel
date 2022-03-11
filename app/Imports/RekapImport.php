4ea9e814a8fdbb57a77b0fd7f602c0a21f570663
franky-tampubolon <garcioemanuel@gmail.com>
2022-03-11 15:34:05 +0700

    edit bugs

--- app/Imports/RekapImport.php
+++ app/Imports/RekapImport.php
@@ -5,7 +5,7 @@ namespace App\Imports;
 use Illuminate\Support\Collection;
 use Maatwebsite\Excel\Concerns\ToCollection;
 
-class RekapImport extends DefaultValueBinder implements ToCollection
+class RekapImport implements ToCollection
 {
     /**
     * @param Collection $collection
